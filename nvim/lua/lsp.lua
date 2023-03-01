5f33fcfd5bf51be6688d06cb8ccb0db3c4d180a3
Mannx <18175956+mannx@users.noreply.github.com>
2023-03-01 00:36:30 -0400

    i3 & nvim update -- lsp

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -2,7 +2,7 @@
 require'lspconfig'.gopls.setup{
 	on_attach = function()
 		vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
-		-- vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0 })
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0 })
 	end,
 
 }
