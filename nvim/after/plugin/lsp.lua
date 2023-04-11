46a72875b9697c92c1a25dc0a6409390b2f335ee
Mannx <18175956+mannx@users.noreply.github.com>
2023-04-11 17:01:27 -0300

    nvim update

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -12,8 +12,9 @@ local cmp_select = {behaviour = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
 	['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
 	['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
-	['<C-Space>'] = cmp.mapping.confirm({ select = true }),
-	-- ['<C-Space>'] = cmp.mapping.complete(),
+	-- ['<C-Space>'] = cmp.mapping.confirm({ select = true }),
+	['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
 })
 
 lsp.set_preferences({
@@ -21,7 +22,8 @@ lsp.set_preferences({
 })
 
 lsp.setup_nvim_cmp({
-	mapping = cmp_mappings
+	mapping = cmp_mappings,
+    completion = {autocomplete = false},
 })
 
 lsp.on_attach(function(client, bufnr)
