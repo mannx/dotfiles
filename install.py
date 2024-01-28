e52cd7c69904ced3a2f6a0be872efb7423549f88
Mannx <18175956+mannx@users.noreply.github.com>
2024-01-28 01:41:18 -0400

    install script udpate. + wallpaper/i3 config

--- install.py
+++ install.py
@@ -1,17 +1,17 @@
 # python script to create symlinks to the selected items
-# TODO: 
+# TODO:
 #   allow selection of which config files to install since some install might not need all
 
 import os
 
 contents = [
     # folder in git directory -> destination folder
-    ( "kitty", "~/.config/kitty" ),
-    ( "nvim", "~/.config/nvim" ),
-    ( "zsh/.zshrc", "~/.zshrc" ),
-    #( "rofi", "~/.config/rofi" ),
-    #( "awesome", "~/.config/awesome" ),
-    #( "i3", "~/.config/i3" ),
+    ("kitty", "~/.config/kitty"),
+    ("nvim", "~/.config/nvim"),
+    ("zsh/.zshrc", "~/.zshrc"),
+    # ( "rofi", "~/.config/rofi" ),
+    # ( "awesome", "~/.config/awesome" ),
+    ("i3", "~/.config/i3"),
 ]
 
 # base path of where we are (determine at runtime?)
@@ -22,5 +22,5 @@ for item in contents:
     dest = item[1]
 
     print("Linking {}...".format(src))
-    cmd = "ln -s {}/{} {}".format(basePath,src,dest)
+    cmd = "ln -s {}/{} {}".format(basePath, src, dest)
     os.system(cmd)
